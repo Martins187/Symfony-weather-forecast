d3bc26db19fc09ffee0d3e30cb32237c2d27eb6e
Martins187 <martins.balcuns1@gmail.com>
2021-12-03 15:48:52 +0200

    bug fixes

--- app/src/Controller/WeatherForecastController.php
+++ app/src/Controller/WeatherForecastController.php
@@ -24,12 +24,18 @@ class WeatherForecastController extends AbstractController
     /**
      * @Route("/", name="index", methods="GET")
      */
-    public function index(WeatherProviderInterface $weatherProvider): Response
+
+    public function index(): Response
+    // public function index(WeatherProviderInterface $weatherProvider): Response
     {
-        $ip = $this->ipAddressService->getIp();
-        $cityName = $this->locationService->getCityName($ip);
-        $weatherForecast = $this->weatherForecastService->getForecast($weatherProvider, $cityName);
-        return new Response($weatherForecast);
+        // $ip = $this->ipAddressService->getIp();
+        // $cityName = $this->locationService->getCityName($ip);
+        // $weatherForecast = $this->weatherForecastService->getForecast($weatherProvider, $cityName);
+        // return new Response($weatherForecast);
+
+        print_r($_SERVER);
+        // $request = Request::createFromGlobals();
+        return new Response($request->getClientIp());
     }
 
      /**
